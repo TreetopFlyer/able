223bebe48db6f204171553733fa8ed79c0c35c13
Seth Trowbridge <seth111@gmail.com>
2023-06-06 17:22:14 -0400

    client started

--- serve.tsx
+++ serve.tsx
@@ -1,88 +1,34 @@
 import * as MIME from "https://deno.land/std@0.180.0/media_types/mod.ts";
 import * as HTTP from "https://deno.land/std@0.177.0/http/server.ts";
-import * as SWCW from "https://esm.sh/@swc/wasm-web@1.3.62";
+import * as Transpile from "./xpile.tsx";
 
-type Configuration = {Proxy:string, Allow:string, Reset:string};
-type ConfigurationArgs = {Proxy?:string, Allow?:string, Reset?:string};
+type Configuration = {Proxy:string, Allow:string, Reset:string, Local:boolean};
+type ConfigurationArgs = {Proxy?:string, Allow?:string, Reset?:string, Local?:boolean};
 
 let Configure:Configuration =
 {
     Proxy: "",
     Allow: "*",
-    Reset: "/clear-cache"
+    Reset: "/clear-cache",
+    Local: false
 };
 export default (config:ConfigurationArgs)=> Configure = {...Configure, ...config};
 
-const TranspileConfig:SWCW.Options = {
-  sourceMaps: true,
-  minify: true,
-  jsc:
-  {
-    minify:
-    {
-        compress: { unused: true },
-        mangle: true
-    },
-    parser:
-    {
-        syntax: "typescript",
-        tsx: true,
-    },
-    transform:
-    {
-      react: { runtime: "automatic" }
-    }
-  },
-}
-const TranspileCache:Map<string, string> = new Map();
-const TranspileFetch =async(inPath:string)=>
-{
-    if(inPath.endsWith(".tsx") || inPath.endsWith(".jsx") || inPath.endsWith(".js") || inPath.endsWith(".mjs"))
-    {
-        const check = TranspileCache.get(inPath);
-        if(check)
-        {
-            return check;
-        }
-        else
-        {
-            try
-            {
-                const resp = await fetch(Configure.Proxy + inPath);
-                const text = await resp.text();
-                const {code, map} = await SWCW.transform(text, TranspileConfig);
-                TranspileCache.set(inPath, code);
-                return code;
-            }
-            catch(e)
-            {
-                return null;
-            }
-        }
-    }
-    else
-    {
-        return false;
-    }
-};
 
-await SWCW.default();
 HTTP.serve(async(req: Request)=>
 {
     const url:URL = new URL(req.url);
 
     if(url.pathname === Configure.Reset)
     {
-        const size = TranspileCache.size;
-        TranspileCache.clear();
-        return new Response(`cache cleared (${size} items)`);
+        return new Response(`cache cleared (${Transpile.Clear()} items)`);
     }
 
-    const lookup = await TranspileFetch(url.pathname);
+    const lookup = await Transpile.Fetch(Configure.Proxy + url.pathname);
     if(lookup === null)
     {
         // error
-        return new Response(`error (see console)`, {status:404, headers:{"content-type":"application/javascript", "Access-Control-Allow-Origin": Configure.Allow, charset:"utf-8"}});
+        return new Response(`transpile error (see console)`, {status:404, headers:{"content-type":"application/javascript", "Access-Control-Allow-Origin": Configure.Allow, charset:"utf-8"}});
     }
     else if(lookup === false)
     {
