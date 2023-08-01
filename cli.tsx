35f8e64a26b210b54a96aa02d771bc6c2c29b716
Seth Trowbridge <seth111@gmail.com>
2023-08-01 15:30:31 -0400

    installer started

--- cli.tsx
+++ cli.tsx
@@ -1,12 +1,11 @@
 import { parse as JSONC } from "https://deno.land/x/jsonct@v0.1.0/mod.ts";
-
+const Root = new URL(`file://${Deno.cwd().replaceAll("\\", "/")}`).toString();
 export async function HuntConfig()
 {
-    const Root = new URL(`file://${Deno.cwd().replaceAll("\\", "/")}`).toString();
     let path:string, resp:Response, text="", json;
     try
     {
-        path = "./deno.json"
+        path = "deno.json"
         resp = await fetch(Root + "/" + path);
         text = await resp.text();
     }
@@ -14,7 +13,7 @@ export async function HuntConfig()
     {
         try
         {
-            path = "./deno.jsonc";
+            path = "deno.jsonc";
             resp = await fetch(Root + "/" + path);
             text = await resp.text();
         }
@@ -26,6 +25,7 @@ export async function HuntConfig()
                 resp = await fetch(path);
                 json = await resp.json();
                 path = json["deno.config"];
+                json = undefined;
                 if(path)
                 {
                     resp = await fetch(Root + "/" + path);
@@ -51,7 +51,18 @@ export async function HuntConfig()
         }
     }
 
-    return {text, path, json};
+    return {path, text, json};
+}
+
+async function Prompt(question: string):Promise<string>
+{
+    const buf = new Uint8Array(1024);
+    await Deno.stdout.write(new TextEncoder().encode(question));
+    const bytes = await Deno.stdin.read(buf);
+    if (bytes) {
+        return new TextDecoder().decode(buf.subarray(0, bytes)).trim();
+    }
+    throw new Error("Unexpected end of input");
 }
 
 export async function SubProcess(args:string[])
@@ -81,5 +92,32 @@ export async function SubProcess(args:string[])
     const status = await child.status;    
 }
 
-const conf = await HuntConfig();
-console.log(conf);
+const config = await HuntConfig();
+if(!config.path)
+{
+    try
+    {
+        const resp = await Prompt("No Deno configuration found. Create one? [y/n]");
+        if(resp == "y")
+        {
+            const pathServer = import.meta.resolve("./");
+            const pathConfig = pathServer + "install__/deno.jsonc";
+
+            console.log(pathServer, pathConfig);
+
+            const resp = await fetch(pathConfig);
+            const text = await resp.text();
+            Deno.writeTextFileSync(Deno.cwd()+"/deno.jsonc", text.replaceAll("{{server}}", pathServer));
+        }
+        else
+        {
+            throw("");
+        }
+    }
+    catch(e)
+    {
+        console.log(e, "(Exiting...)");
+        Deno.exit();
+    }
+}
+console.log(config);
